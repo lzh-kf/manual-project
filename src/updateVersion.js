b6b445c96063fb9985aa90017754aeccb5a1e07c
lizhenhui <15088792512@163.com>
2024-05-31 17:23:49 +0800

    feat:v12

--- src/updateVersion.js
+++ src/updateVersion.js
@@ -1,4 +1,16 @@
 import axios from 'axios'
+import { ElMessageBox,ElMessage } from 'element-plus'
+let isFirstLoad = true
+const versionInfo = {
+    cacheValue:null,
+    get value(){
+        if(versionInfo.cacheValue) {
+            return  versionInfo.cacheValue
+        }
+        versionInfo.cacheValue = localStorage.getItem('version')
+        return versionInfo
+    }
+}
 const managementVersion = () => {
     axios('/manual-project/dist/version.json', {
         headers:{
@@ -6,27 +18,39 @@ const managementVersion = () => {
         }
     }).then(res=> {
         const { version } = res.data
-        const localVersion = localStorage.getItem('version')
-        if(localVersion) {
-            if(localVersion!=version) {
-                if (confirm("发现新版本，是否更新?")) {
-                    localStorage.setItem('version', version)
-                    setTimeout(()=> {
-                    alert('更新新版本了!!!')
-                        location.reload()
-                    },3000)
-                  } else {
-                    alert('你取消了版本更新')
-                  }
-            }
-        } else {
+        if(isFirstLoad) {
+            isFirstLoad = false
             localStorage.setItem('version', version)
-        } 
+        } else {
+            const localVersion = versionInfo.value
+            if(localVersion!=version) {
+                ElMessageBox.confirm('发现新版本，是否更新?',
+                    {
+                      confirmButtonText: '确认',
+                      cancelButtonText: '取消',
+                      type: '更新版本确认框？',
+                    }
+                  ).then(() => {
+                      ElMessage({
+                        type: 'success',
+                        message: '更新新版本了!!!',
+                        onClose(){
+                            localStorage.setItem('version', version)
+                            location.reload()
+                        }
+                      })
+                    }).catch(() => {
+                      ElMessage({
+                        type: 'info',
+                        message: '你取消了版本更新',
+                      })
+                    })
+            }
+        }
        })
 }
 if(process.env.NODE_ENV === 'production') {
     setInterval(()=> {
-        console.log('我是最新的代码')
         managementVersion()
-    }, 1000 * 5)
+    }, 1000 * 10)
 }
