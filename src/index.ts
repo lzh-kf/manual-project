24118effa1eba57ba6d5e16ef226c88efc80609b
lizhenhui <15088792512@163.com>
2024-05-31 15:16:30 +0800

    feat:增加版本信息json

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import router from './router'
 import 'normalize.css'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './updateVersion'
 const app = createApp(App)
 app.use(router)
 app.use(createPinia())
---
+++ src/updateVersion.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+const managementVersion = () => {
+    axios('/version.json').then(res=> {
+        const { version } = res
+        const localVersion = localStorage.getItem('version')
+        if(localVersion) {
+            if(localVersion==version) {
+                if (window.confirm("发现新版本，是否更新?")) {
+                    window.location.reload()
+                  } else {
+                    alert('你取消了版本更新')
+                  }
+            }
+        } else {
+            localStorage.setItem('version', version)
+        } 
+       })
+}
+if(process.env.NODE_ENV === 'production') {
+    setInterval(()=> {
+        managementVersion()
+    },1000*60)
+}
